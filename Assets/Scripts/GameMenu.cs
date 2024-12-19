9b81416d0189c3e66175f2aac42bcc5d59d72984
JustTobias <t.fernhout@outlook.com>
2024-12-19 14:27:56 +0100

    updated menu to show and update player stats

--- Assets/Scripts/GameMenu.cs
+++ Assets/Scripts/GameMenu.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameMenu : MonoBehaviour
 {
     public GameObject theMenu;
+
+    private CharStats[] playerStats;
+
+    public Text[] nameText, hpText, mpText, lvlText, expText;
+    public Slider[] expSlider;
+    public Image[] charImage;
+    public GameObject[] charStatHolder;
+
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +30,22 @@ public class GameMenu : MonoBehaviour
                 GameManager.instance.gameMenuOpen = false;
             } else {
                 theMenu.SetActive(true);
+                UpdateMainStats();
                 GameManager.instance.gameMenuOpen = true;
+            }
+        }
+    }
+
+    public void UpdateMainStats() {
+        playerStats = GameManager.instance.playerStats;
+
+        for(int i = 0; i < playerStats.Length; i++) {
+            if (playerStats[i].gameObject.activeInHierarchy) {
+                charStatHolder[i].SetActive(true);
+
+                nameText[i].text = playerStats[i].charName;
+            } else {
+                charStatHolder[i].SetActive(false);
             }
         }
     }
