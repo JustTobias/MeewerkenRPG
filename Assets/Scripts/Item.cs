328370432264d790744e8423680bdfe75dff46fc
JustTobias <t.fernhout@outlook.com>
2025-01-22 14:56:54 +0100

    created some items, layout item panel

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [Header("Item Type")]
+    public bool isItem;
+    public bool isWeapon;
+    public bool isArmour;
+
+    [Header("Item Info")]
+    public string itemName;
+    public string itemDesc;
+    public int value;
+    public Sprite itemSprite;
+
+    [Header("Item Details")]
+    public int amountToChange;
+    public bool affectHP, affectMP, affectStr;
+
+    [Header("Weapon/Armour details")]
+    public int weaponStrength;
+    public int armourStrength;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
